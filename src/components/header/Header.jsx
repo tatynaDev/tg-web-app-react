5c5d23ef2bcf3593b060a9ccccab0584470e9e4c
tatynaerkinbek <tatynaerkinbek@gmail.com>
2023-06-05 16:32:14 +0600

    init2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from "../button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css'
 
 const Header = () => {
     const {user, onClose} = useTelegram()
 
     return (
-        <div className={'header'}>
+        <div className='header'>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>{user?.username}</span>
         </div>
